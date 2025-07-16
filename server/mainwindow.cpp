eeeeb3b68186a4e4e718ac010bfd50a4b3bc61e9
Ilya <ilyakud667@gmail.com>
2025-07-16 22:17:05 +0300

    Server Base and Client Base

--- server/mainwindow.cpp
+++ server/mainwindow.cpp
@@ -6,9 +6,51 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    startup();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::startup()
+{
+    server = new QTcpServer(this);
+
+    connect(server,&QTcpServer::newConnection,this,&MainWindow::newConnection);
+
+    server->listen(QHostAddress::Any, 4444);
+}
+
+void MainWindow::newReadyRead()
+{
+    QByteArray data = socket->readAll();
+
+    if(data.startsWith("/reg"))
+    {
+        QString data_text = data.mid(5);
+        QStringList splitedData = data_text.split("#");
+
+        User *user = new User(splitedData[0],splitedData[1]);
+        users.push_back(user);
+    }
+}
+
+void MainWindow::newConnection()
+{
+    socket = server->nextPendingConnection();
+
+    connect(socket,&QTcpSocket::readyRead,this, &MainWindow::newReadyRead);
+
+    qDebug() << "Connected: " << socket->peerAddress();
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    for(auto it : users)
+    {
+        it->Get();
+    }
+}
+
--- server/mainwindow.h
+++ server/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,8 +18,39 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    class User
+    {
+    public:
+
+        User(QString id, QString pass)
+        {
+            this->id = id;
+            this->pass = pass;
+
+        }
+
+        void Get()
+        {
+            qDebug() << "Id: " << id << " Pass: " << pass;
+        }
+
+        QString id;
+        QString pass;
+    };
+
+private slots:
+    void startup();
+    void newReadyRead();
+    void newConnection();
+
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QTcpServer *server;
+    QTcpSocket *socket;
+    QVector<User*> users;
+
 };
+
 #endif // MAINWINDOW_H
